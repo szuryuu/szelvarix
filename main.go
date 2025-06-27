aba1f0e4c1cca82c1395416644a3b8af871b1ca6
shfwnz <ilhamdzaky2007@gmail.com>
2025-06-27 15:44:03 +0700

    feat: epic narrative

--- main.go
+++ main.go
@@ -9,9 +9,47 @@ import (
 var name string
 
 func main() {
-	fmt.Print("Welcome adventurers, please give your name: ")
+	// ASCII-art Tree of Life
+	fmt.Print(`
+            .        +          .      .          .
+     .            _        .                    .
+  ,              /;-._,-.____        ,-----.__
+ ((        .    (_:#::_.:::. '-._   /:, /-._, '._,
+  '                 \   _|'"=:_::.' );  \ __/ /
+                      ,    './  \:. '.   )==-'  .
+    .      ., ,-=-.  ,\, +#./'   \:.  / /           .
+.           \/:/'-' , ,\ ' + "" +    ):  \ /_  -o
+       .    /:+- - + +- : :- + + -:'  /(o-) \)     .
+  .      ,=':  \    ' '/ + "" +  ' , , ,:' '--".--"---._/7
+   '.   (    \: \,-._' ' + '\, ,"   _,--._,---":.__/
+              \:  '  X' _| _,\/'   .-'
+.               ":._:'\____  /:'  /      .           .
+                    \::.  :\/:'  /              +
+   .                 '.:.  /:'  }      .
+           .           ):_(:;   \           .
+                      /:. _/ ,  |
+    .                (|::.     , + "" +                   .
+     .                |::.    {\                  .
+                      |::.\  \ '.
+                      |:::(\    |
+              O       |:::/{ }  |                  (o
+               )  ___/#\::'/ (O "==._____   O, (O  /'
+          ~~~w/w~"~~,\ '/,-(~'"~~~~~~~~"~o~\~/~w|/~
+dew   ~~~~~~~~~~~~~~~~~~~~~~~\\W~~~~~~~~~~~~\|/~~
+`)
+
+	// Epic narrative
+	fmt.Println("✨ The roots of *Szelvarix* stir gently as you approach...")
+	fmt.Println("🌿 A soft voice echoes from within the sacred bark:")
+	fmt.Println()
+	fmt.Println("    \"State your name, brave wanderer of the realm:\"")
+	fmt.Print("    > ")
+
 	fmt.Scanln(&name)
+
 	nameToUpperCase := strings.ToUpper(name)
 	greet := greetings.Hello(nameToUpperCase)
+	fmt.Println()
+	fmt.Println("🌳 The leaves shimmer as your essence is read...")
 	fmt.Println(greet)
 }
